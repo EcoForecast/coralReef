74e090c480e87d1e7dd65166355e2e299ea1a90c
Kathryn Wheeler <kiwheel@bu.edu>
2019-03-18 15:05:18 -0400

    Added script to pull and process WHOI SST data and a script to plot the WHOI SST data. Updated the mainCoralReefForecast to include calls to the new files.

---
+++ getWHOI_SST.R
@@ -0,0 +1,38 @@
+##Script to download whoi sea surface temperature
+##'
+##' @param years vector of years to download
+##' @import ncdf4
+##' @import RCurl
+##' @import XML
+##' @import devtools
+##' @export
+getWHOI_SST <- function(years){
+timeSeqs <- c("01:00:00","04:00:00","07:00:00","10:00:00","13:00:00","16:00:00","19:00:00","22:00:00")
+lat <- 24.6160 #For Looe Key
+lon <- 360-81.4000
+
+SSTvals <- numeric()
+dateVals <- character()
+
+for(y in years){
+  WHOI_Folder <- paste("https://www.ncei.noaa.gov/data/sea-surface-temperature-whoi/access/",y,"/",sep="")
+  WHOI_html <- getURL(WHOI_Folder)
+  dat <- readHTMLTable(WHOI_html)[[1]]
+  dat <- dat[!is.na(dat$Name),]
+  dat <- dat[dat$Name!="Parent Directory",]
+  for(f in 1:nrow(dat)){
+    subFileName <- as.character(dat$Name[f])
+    WHOI_fileName <- paste(WHOI_Folder,subFileName,sep="")
+    system(paste("wget",WHOI_fileName))
+    WHOIdat <- nc_open(subFileName)
+    SST <- ncvar_get(WHOIdat,"sea_surface_temperature")
+    mth <- substr(strsplit(subFileName,"_")[[1]][4],6,7)
+    dy <- substr(strsplit(subFileName,"_")[[1]][4],8,9)
+    for(t in 1:length(timeSeqs)){
+      SSTvals <- c(SSTvals,SST[lat,lon,t])
+      dateVals <- c(dateVals,paste(y,"-",mth,"-",dy," ",timeSeq[t],sep=""))
+    }
+  }
+}
+  return(cbind(dateVals,SSTvals))
+}  
--- mainCoralReefForecast.R
+++ mainCoralReefForecast.R
@@ -1,5 +1,12 @@
 source("scrapeCoral.R")
 source("plotCoralBleachingData.R")
+source("getWHOI_SST.R")
+source("plotWHOI_SST.R")
 
+##Coral Bleaching Data
 FLdat <- scrapeCoral()
 plotCoralBleachingData(FLdat=FLdat)
+
+##WHOI Sea Surface Temperature Data (Note: this takes a long time to run)
+SSTdat <- getWHOI_SST(years=seq(1988,2018,1))
+plotWHOI_SST(SSTdat = SSTdat)
