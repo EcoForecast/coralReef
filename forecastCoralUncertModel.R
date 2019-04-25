0d78f6c6891b2d4e7cd3b6615b65c7259384f10a
Shelby <sundqshe@bu.edu>
2019-04-25 18:53:45 -0400

    tested

--- forecastCoralUncertModel.R
+++ forecastCoralUncertModel.R
@@ -5,18 +5,20 @@
 ##' @param beta1 Intercept
 ##' @param year Year random effects
 ##' @param reg Region random effects
+##' @param rec Recovery
 ##' @param Q Process error (default = 0 for deterministic runs)
 ##' @param n Size of Monte Carlo ensemble
 ##' @param S Heat stress covariate
 ##' @export
-forecastCoralUncertModel <- function(IC,beta0,beta1,year,reg,Q=0,n,S){
+ForecastCoralUncertModel <- function(IC,beta0,beta1,year,reg,rec,Q=0,n,S){
+  nr=5
+  nt=5
   x <- array(dim=c(nr,nt,n))
   for(r in 1:nr){
     Xprev <- IC[r]
-    print(Xprev)
   for(t in 1:nt){
-    mu <- beta0 * Xprev + beta1 * S[r,t]+ year[t] + reg[r] ##Process model
-    print(mu)
+    layover=rec * Xprev
+    mu <- beta0 * layover + beta1 * S[r,t]+ year[t] + reg[r] ##Process model
     x[r,t,] <- rlnorm(n,mu,Q)
     Xprev <- x[r,t,]
     #x[r,t] <-  dnorm(mu[r,t],tau_proc)
