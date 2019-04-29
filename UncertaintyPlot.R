cbb6a7204fee2aee802368515f6670c0b9a9705b
Shelby <sundqshe@bu.edu>
2019-04-29 12:38:55 -0400

    added legend

--- UncertaintyPlot.R
+++ UncertaintyPlot.R
@@ -22,15 +22,18 @@ for(r in 1:5) {
   bp=out.mat2[,col]
   bp.ci=apply(bp, 2,quantile,c(0.025,0.5,0.975))
   plot(t1, bp.ci[2,], xlim=c(1988, 2016), ylim=c(0, 1.1),
-       xlab="year", ylab="percent bleaching", main=loc[r])
+       xlab="year", ylab="percent bleaching", main=(paste(loc[r], "Uncertainty", sep=" ")))
   ecoforecastR::ciEnvelope(t1,bp.ci[1,],bp.ci[3,],col=col.alpha("lightBlue", .6))
   lines(t1, bp.ci[2,], col="blue")
-  lines(t2,N.det[r,,1],lwd=0.5, col="purple")
   ecoforecastR::ciEnvelope(t2,N.IPDEA.ci[r,,1],N.IPDEA.ci[r,,3],col=col.alpha("orange",trans))
   ecoforecastR::ciEnvelope(t2,N.IPDE.ci[r,,1],N.IPDE.ci[r,,3],col=col.alpha("blue",trans))
   ecoforecastR::ciEnvelope(t2,N.IPD.ci[r,,1],N.IPD.ci[r,,3],col=col.alpha("green",trans))
   ecoforecastR::ciEnvelope(t2,N.IP.ci[r,,1],N.IP.ci[r,,3],col=col.alpha("red",trans))
   ecoforecastR::ciEnvelope(t2,N.I.ci[r,,1],N.I.ci[r,,3],col=col.alpha("black",trans))
+  lines(t2,N.det[r,,1],lwd=0.5, col="purple")
+  legend("topleft", legend=c("IC", "Param", "Driver", "Process", "Random"), 
+         fill=c('black','red','green','blue','orange'))
+  #Add legend
 }
 }
 #add regions title (figure out which is which)
