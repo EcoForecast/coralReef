b9b89f6bfbd2ae1f59df8c3e57e6693a5c624ee3
Kathryn Wheeler <kiwheel@bu.edu>
2019-05-06 17:17:49 -0400

    merging

--- UncertaintyPlot.R
+++ UncertaintyPlot.R
@@ -2,38 +2,6 @@
 ##'@param out.mat2 Model predictions at t = 1-16
 
 plot_uncertainty <- function(param.ci, out.mat2) {
-<<<<<<< HEAD
-  t1=1988:2003
-  t2=2004:2016
-  t=1988:2016
-  trans=1 #transparency
-  
-  N.det = as.array(param.ci[[1]])
-  N.I.ci = as.array(param.ci[[2]])
-  N.IP.ci = as.array(param.ci[[3]])
-  N.IPD.ci = as.array(param.ci[[4]])
-  N.IPDE.ci = as.array(param.ci[[5]])
-  N.IPDEA.ci = as.array(param.ci[[6]])
-  
-  loc=c("Lower Keys","Middle Keys","Upper Keys","Biscayne Bay","Dry Tortugas")
-  for(r in 1:5) {
-    col=seq(0, 79, by=5)
-    col=col+r
-    bp=out.mat2[,col]
-    bp.ci=apply(bp, 2,quantile,c(0.025,0.5,0.975))
-    plot(t1, bp.ci[2,], xlim=c(1988, 2016), ylim=c(0, 1.1),
-         xlab="year", ylab="percent bleaching", main=loc[r])
-    ecoforecastR::ciEnvelope(t1,bp.ci[1,],bp.ci[3,],col=col.alpha("lightBlue", .6))
-    lines(t1, bp.ci[2,], col="blue")
-    ecoforecastR::ciEnvelope(t2,N.IPDEA.ci[r,,1],N.IPDEA.ci[r,,3],col=col.alpha("orange",trans))
-    ecoforecastR::ciEnvelope(t2,N.IPDE.ci[r,,1],N.IPDE.ci[r,,3],col=col.alpha("blue",trans))
-    ecoforecastR::ciEnvelope(t2,N.IPD.ci[r,,1],N.IPD.ci[r,,3],col=col.alpha("green",trans))
-    ecoforecastR::ciEnvelope(t2,N.IP.ci[r,,1],N.IP.ci[r,,3],col=col.alpha("red",trans))
-    lines(t2,N.det[r,,1],lwd=0.5, col="purple")
-    ecoforecastR::ciEnvelope(t2,N.I.ci[r,,1],N.I.ci[r,,3],col=col.alpha("black",trans))
-  }
-}
-=======
 t1=1988:2003
 t2=2004:2016
 t=1988:2016
@@ -68,4 +36,4 @@ for(r in 1:5) {
 }
 }
 #add regions title (figure out which is which)
->>>>>>> cbb6a7204fee2aee802368515f6670c0b9a9705b
+
